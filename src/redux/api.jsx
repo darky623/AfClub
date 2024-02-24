2d1f0720062b02b986df9e5639b440d6161d8c88
Max Arbakov <fefaodessa@gmail.com>
2024-02-24 19:54:21 +0200

    fix2/2

--- src/redux/api.jsx
+++ src/redux/api.jsx
@@ -1,12 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const apiUrl =
-  process.env.NODE_ENV === "production"
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "production"
     ? process.env.NEXT_PUBLIC_API_URL_PROD
     : process.env.NEXT_PUBLIC_API_URL_DEV;
 console.log(process.env.NEXT_PUBLIC_VERCEL_ENV);
-console.log(process.env.NEXT_PUBLIC_API_URL_PROD);
-console.log(process.env.NEXT_PUBLIC_API_URL_DEV);
 
 export const api = createApi({
   reducerPath: "api",
