5694c6c1700574cd321269491083d3945fd851a8
Max Arbakov <fefaodessa@gmail.com>
2024-02-24 19:21:27 +0200

    test

--- src/redux/api.jsx
+++ src/redux/api.jsx
@@ -4,6 +4,7 @@ const apiUrl =
   process.env.NODE_ENV === "production"
     ? process.env.NEXT_PUBLIC_API_URL_PROD
     : process.env.NEXT_PUBLIC_API_URL_DEV;
+console.log(process.env.NODE_ENV);
 console.log(process.env.NEXT_PUBLIC_API_URL_PROD);
 console.log(process.env.NEXT_PUBLIC_API_URL_DEV);
 
