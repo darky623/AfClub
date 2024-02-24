e667baaecc636d51439373e703073f5be354df8c
Max Arbakov <fefaodessa@gmail.com>
2024-02-24 19:33:52 +0200

    fix 2/0

--- src/redux/api.jsx
+++ src/redux/api.jsx
@@ -2,8 +2,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const apiUrl =
   process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_API_URL_PROD
-    : process.env.NEXT_PUBLIC_API_URL_DEV;
+    ? process.env.NEXT_PUBLIC_API_URL_DEV
+    : process.env.NEXT_PUBLIC_API_URL_PROD;
 
 console.log(process.env.NODE_ENV);
 console.log(process.env.NEXT_PUBLIC_API_URL_PROD);
