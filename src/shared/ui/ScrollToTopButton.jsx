88a358392fd3be5793aa4d5940c5cda0bff5aa0e
Max Arbakov <fefaodessa@gmail.com>
2024-03-29 12:13:28 +0200

    fixMetod

--- src/shared/ui/ScrollToTopButton.jsx
+++ src/shared/ui/ScrollToTopButton.jsx
@@ -1,12 +1,15 @@
 import { Button, FloatButton } from "antd";
 import { UpOutlined } from "@ant-design/icons";
+import React, { useRef } from "react";
 import s from "./ScrollToTopButton.module.scss";
 
 const ScrollToTopButton = () => {
+  const buttonRef = useRef(null);
   return (
     <FloatButton.BackTop visibilityHeight={400}>
       <Button
-        type="Default"
+        ref={buttonRef}
+        type="default"
         shape="circle"
         icon={<UpOutlined />}
         size="large"
