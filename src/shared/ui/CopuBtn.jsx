5244cca2ca6f57113aa15f25a584e2039710a19f
Max Arbakov <fefaodessa@gmail.com>
2024-04-23 13:03:05 +0300

    fix svg

--- src/shared/ui/CopuBtn.jsx
+++ src/shared/ui/CopuBtn.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Image from "next/image";
-import s from "./EditingBtn.module.scss";
+import { CopyOutlined } from "@ant-design/icons";
+import s from "./DeletBtn.module.scss";
 
 const CopyBtn = ({ onClick }) => {
   return (
-    <button onClick={onClick} className={s.editing_btn}>
-      <Image src={"/copy.svg"} width={14} height={14} alt="copy" />
+    <button onClick={onClick} className={s.delete_btn}>
+      <CopyOutlined />
     </button>
   );
 };
