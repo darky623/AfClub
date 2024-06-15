be6d1f979cff6c5ac42f80828cd4463b1b6400e2
Max Arbakov <fefaodessa@gmail.com>
2024-06-15 22:52:57 +0300

    fix color QuestionnaireClient

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,26 @@
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
 import Layout from "../components/Layout/Layout";
+import { useRouter } from "next/router";
 import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  const isQuestionnaireClientPage =
+    router.pathname === "/QuestionnaireClient/QuestionnaireClient";
+
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <div
+        className={
+          isQuestionnaireClientPage ? "questionnaire-client-background" : ""
+        }
+      >
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
     </Provider>
   );
 }
